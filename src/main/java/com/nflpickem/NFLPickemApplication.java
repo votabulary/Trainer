d4bcaa36e3e439aa704fd251c925478088aeb0c1
jdschmitt <jason.schmitt@gmail.com>
2014-10-06 22:33:48 -0500

    Generated JOOQ sources for current schema state

--- src/main/java/com/nflpickem/NFLPickemApplication.java
+++ src/main/java/com/nflpickem/NFLPickemApplication.java
@@ -3,15 +3,19 @@ package com.nflpickem;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.nflpickem.model.dao.PlayerDAO;
 import com.nflpickem.view.resource.AdminPlayerResource;
 import io.dropwizard.Application;
+import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.skife.jdbi.v2.DBI;
 
 public class NFLPickemApplication extends Application<NFLPickemConfiguration> {
 
     private Injector injector;
     private Environment environment;
+    private NFLPickemConfiguration configuration;
 
     public static void main(String[] args) throws Exception {
         new NFLPickemApplication().run(args);
@@ -30,6 +34,7 @@ public class NFLPickemApplication extends Application<NFLPickemConfiguration> {
     @Override
     public void run(NFLPickemConfiguration configuration, Environment environment) throws Exception {
         this.environment = environment;
+        this.configuration = configuration;
         injector = Guice.createInjector();
         environment.jersey().register(injector.getInstance(AdminPlayerResource.class));
 
@@ -38,5 +43,13 @@ public class NFLPickemApplication extends Application<NFLPickemConfiguration> {
 
     private void configureSerialization() {
         environment.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+    private void configureDatabase() throws ClassNotFoundException {
+        final DBIFactory factory = new DBIFactory();
+        final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "msyql");
+        final PlayerDAO dao = jdbi.onDemand(PlayerDAO.class);
+//        environment.jersey().register(new UserResource(dao));
+        // TODO Add player resource here...make it injectible
     }
 }
--- src/main/java/com/nflpickem/NFLPickemConfiguration.java
+++ src/main/java/com/nflpickem/NFLPickemConfiguration.java
@@ -2,9 +2,22 @@ package com.nflpickem;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
 public class NFLPickemConfiguration extends Configuration {
+    @Valid
+    @NotNull
+    @JsonProperty
+    private DataSourceFactory database = new DataSourceFactory();
+
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
     @NotEmpty
     private String something;
 
